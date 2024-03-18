37a854406f2fbfe1af59f01dfea71e5da06b79fe
Ryan Wittkopf <rwa@sfu.ca>
2024-03-17 17:32:39 -0700

    Got NFC to work

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -18,10 +18,12 @@ int main() {
     TextToSpeech textToSpeech;
     ShutdownManager shutdownManager;
     Network network(&shutdownManager, &textToSpeech, &audioMixer);
+    NFCReader reader("/dev/i2c-2", 0x24);
 
     while(!shutdownManager.isShutdown()) {
-        std::this_thread::sleep_for(std::chrono::seconds(4));
-        audioMixer.queueSound(ENGLISH);
+        std::string uid = reader.waitForCardAndReadUID();
+        std::cout << "UID = " << uid << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     shutdownManager.waitForShutdown();
--- hal/src/nfc.cpp
+++ hal/src/nfc.cpp
@@ -31,7 +31,7 @@ bool NFCReader::sendCommandAndWaitForResponse(unsigned char* command, int comman
         return false;
     }
 
-    usleep(25000); 
+    usleep(250000); 
 
     if (read(fileDescriptor, response, responseLength) > 0) {
         return true;
@@ -42,6 +42,20 @@ bool NFCReader::sendCommandAndWaitForResponse(unsigned char* command, int comman
 }
 
 std::string NFCReader::waitForCardAndReadUID() {
+        unsigned char setupCommand[] = {
+        0x00, // Reserved byte
+        0x00, // Reserved byte
+        0xFF, // Start of packet
+        0x03, // Packet length
+        0xFD, // Length checksum (0x100 - 0x03 = 0xFD)
+        0xD4, // Data Exchange command
+
+        0x14, // SAMConfiguration command
+        0x01, // 
+        
+        0x17, // Checksum for the command bytes (0x100 - 0xD4 - 0x14 - 0x01 = 0x17)
+        0x00, // Reserved byte
+    };
         unsigned char detectCardCommand[] = {
         0x00, // Reserved byte
         0x00, // Reserved byte
@@ -49,18 +63,37 @@ std::string NFCReader::waitForCardAndReadUID() {
         0x04, // Packet length
         0xFC, // Length checksum (0x100 - 0x04 = 0xFC)
         0xD4, // Data Exchange command
+
         0x4A, // InListPassiveTarget command
         0x01, // Max number of targets
         0x00, // Baud rate
-        0xE1, // Checksum for the command bytes (0x100 - 0x1F = 0xE1)
+
+        0xE1, // Checksum for the command bytes (0x200 - 0xD4 - 0x4a - 0x01 - 0x00 = 0xE1)
+        0x00, // Reserved byte
     };
     unsigned char response[256];
     std::string uid;
 
+
+    memset(response, 0, sizeof(response));
+
+    if (sendCommandAndWaitForResponse(setupCommand, sizeof(setupCommand), response, sizeof(response))) {
+        for(int i = 0; i < 23; i++) {
+            std::cout << "0x" << std::hex << (int)response[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
     while (true) {
+
         memset(response, 0, sizeof(response));
 
         if (sendCommandAndWaitForResponse(detectCardCommand, sizeof(detectCardCommand), response, sizeof(response))) {
+            std::cout << "Response" << std::endl;
+            for(int i = 0; i < 23; i++) {
+                std::cout << "0x" << std::hex << (int)response[i] << " ";
+            }
+            std::cout << std::endl;
             for (int i = 19; i < 23; i++) { // Assuming 4-byte UID
                 uid += (i > 19 ? ":" : "") + std::to_string((int)response[i]);
             }
