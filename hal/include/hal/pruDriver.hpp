c8f4776ec4a87773b0d25020070d3e2a5942a0b3
Ryan Wittkopf <rwa@sfu.ca>
2024-04-11 19:05:42 -0700

    Read joystick from PRU

---
+++ hal/include/hal/pruDriver.hpp
@@ -0,0 +1,18 @@
+#ifndef _PRU_DRIVER_HPP_
+#define _PRU_DRIVER_HPP_
+
+#include "sharedDataStruct.h"
+
+class PruDriver {
+public:
+    PruDriver();
+    ~PruDriver();
+    bool isRightPressed();
+private:
+    volatile sharedMemStruct_t *pSharedPru0;
+
+    volatile void* getPruMmapAddr(void);
+    void freePruMmapAddr(volatile void* pPruBase);
+};
+
+#endif
---
+++ hal/src/pruDriver.cpp
@@ -0,0 +1,65 @@
+#include "hal/pruDriver.hpp"
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+#include <iostream>
+
+// General PRU Memomry Sharing Routine
+// ----------------------------------------------------------------
+#define PRU_ADDR      0x4A300000   // Start of PRU memory Page 184 am335x TRM
+#define PRU_LEN       0x80000      // Length of PRU memory
+#define PRU0_DRAM     0x00000      // Offset to DRAM
+#define PRU1_DRAM     0x02000
+#define PRU_SHAREDMEM 0x10000      // Offset to shared memory
+#define PRU_MEM_RESERVED 0x200     // Amount used by stack and heap
+
+// Convert base address to each memory section
+#define PRU0_MEM_FROM_BASE(base) ( (uintptr_t)(base) + PRU0_DRAM + PRU_MEM_RESERVED)
+#define PRUSHARED_MEM_FROM_BASE(base) ( (base) + PRU_SHAREDMEM)
+
+PruDriver::PruDriver() {
+    system("config-pin P8_15 pruin");
+    volatile void *pPruBase = getPruMmapAddr();
+    pSharedPru0 = (volatile sharedMemStruct_t *)(PRU0_MEM_FROM_BASE(pPruBase));
+}
+
+PruDriver::~PruDriver() {
+    freePruMmapAddr((void*) pSharedPru0);
+}
+
+bool PruDriver::isRightPressed() {
+    return pSharedPru0->isRightPressed;
+}
+
+// Return the address of the PRU's base memory
+volatile void* PruDriver::getPruMmapAddr(void)
+{
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (fd == -1) {
+        perror("ERROR: could10 not open /dev/mem");
+        exit(EXIT_FAILURE);
+    }
+
+    // Points to start of PRU memory.
+    volatile void* pPruBase = mmap(0, PRU_LEN, PROT_READ | PROT_WRITE, MAP_SHARED, fd, PRU_ADDR);
+    if (pPruBase == MAP_FAILED) {
+        perror("ERROR: could not map memory");
+        exit(EXIT_FAILURE);
+    }
+    close(fd);
+
+    return pPruBase;
+}
+
+void PruDriver::freePruMmapAddr(volatile void* pPruBase)
+{
+    if (munmap((void*) pPruBase, PRU_LEN)) {
+        perror("PRU munmap failed");
+        exit(EXIT_FAILURE);
+    }
+}
